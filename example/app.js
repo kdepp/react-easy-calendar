f5b924ad5305d3a2b7d0f7c642eeb66e77d35c77
KDepp <kdepp.cd@gmail.com>
2016-04-20 12:50:46 +0800

    opt

--- example/app.js
+++ example/app.js
@@ -3,6 +3,7 @@ import {render} from 'react-dom';
 
 import * as c from '../src/common/calendar_utils';
 import RawCalendar from '../src/raw_calendar';
+import SimpleCalendar from '../src/simple_calendar';
 
 let raw_calendar_config = {
     calendar_mode: 1,
@@ -20,7 +21,14 @@ let raw_calendar_config = {
 
 render((
     <div>
-        <h3>Raw Calendar</h3>
-        <RawCalendar {...raw_calendar_config} />
+        <div>
+            <h3>Raw Calendar</h3>
+            <RawCalendar {...raw_calendar_config} />
+        </div>
+
+        <div>
+            <h3>Simple Calendar</h3>
+            <SimpleCalendar {...raw_calendar_config} />
+        </div>
     </div>
 ), document.getElementById('app_root'));
