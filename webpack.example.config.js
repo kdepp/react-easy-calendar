4e83020c709d7bc073c5f2cb1ec036a69308db06
KDepp <kdepp.cd@gmail.com>
2016-04-21 13:27:55 +0800

    opt

--- webpack.example.config.js
+++ webpack.example.config.js
@@ -4,21 +4,19 @@ var webpack = require('webpack'),
 module.exports = {
     entry: {
         bundle: [
-            './src/index.js'
+            './example/app.js'
         ]
     },
     output: {
         path: path.join(__dirname, 'lib'),
         filename: 'calendar.js',
-        library: 'react-easy-calendar',
-        libraryTarget: 'umd'
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: ['babel?presets[]=stage-0&presets[]=es2015']
+                loaders: ['babel?presets[]=stage-0&presets[]=es2015&presets[]=react']
             }
         ]
     }
